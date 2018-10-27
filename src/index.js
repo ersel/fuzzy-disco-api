abb25d4bfbc90cf11bc6b1bd5160df56c86074ba
Ersel Aker <erselaker@gmail.com>
2018-10-27 16:24:28 +0100

    list routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const expressListRoutes   = require('express-list-routes');
+const router = express.Router();
 
 const { auth, users, secrets } = require('./routes');
 const authenticate = require('./middleware/authenticate');
@@ -13,9 +15,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/auth', auth);
-app.use('/users', users);
-app.use('/secrets', authenticate, secrets)
+router.use('/auth', auth);
+router.use('/users', users);
+router.use('/secrets', authenticate, secrets);
+app.use(router);
+
+router.stack.forEach(r => {
+  const prefix = r.regexp.toString().replace(/\W/g, '').slice(0, -1)
+  expressListRoutes({ prefix: `${prefix}` }, '', r.handle);
+})
+
 
 mongoose.connect(process.env.DATABASE_URL, () => {
   console.log('connected to database');
