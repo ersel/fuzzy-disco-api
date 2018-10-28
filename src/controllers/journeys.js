f92db3ec3ca39da9aeacc323bc501209065bc121
Ersel Aker <erselaker@gmail.com>
2018-10-28 05:50:23 +0000

    return journey info

--- src/controllers/journeys.js
+++ src/controllers/journeys.js
@@ -1,9 +1,27 @@
 const Journey = require('../models/journey');
+const axios = require('axios');
+
+axios.defaults.baseURL = 'http://transportapi.com/v3/uk';
+
+const generateRequestURI = ({ start, end, operator }) => (
+  `/train/station/${start}/live.json?calling_at=${end}&operator=${operator}&type=departure&app_id=${process.env.TRANSPORT_API_APP_ID}&app_key=${process.env.TRANSPORT_API_KEY}`
+);
+
+const isLate = time => departure => departure.aimed_departure_time === time && departure.status === 'LATE';
+const filterNulls = d => d.filter(x => x !== null)
 
 const index = (req, res) => {
-  Journey.find({ userId: req.authorizer.id })
+  return Journey.find({ userId: req.authorizer.id })
     .then((journeys) => {
-      res.status(200).json(journeys);
+      return Promise.all(journeys.map(generateRequestURI)).then(URIs => 
+        Promise.all(URIs.map(axios.get))
+      ).then(responses => {
+        const results = responses.map((response, idx) => ({
+          ...response.data,
+          ...journeys[idx].toObject()
+        }))
+        res.status(200).json(results)
+      }).catch(e => console.log(e))
     })
     .catch((error) => {
       console.log(error);
